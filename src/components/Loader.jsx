ba73f608a2eaa82fef80c9e599a03e12463534de
Leyder Gallego Cano <leyder.gallego@gmail.com>
2023-07-04 15:50:55 -0500

    Adicion visual a la imagen cargada y otras mejoras funcionales y visuales

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
+import { BarLoader } from 'react-spinners'; // Importa el componente BarLoader
 
 const Loader = () => {
-  return <div>Loading...</div>;
+  return <BarLoader color="#2F80ED" height={4} width="100%" />; // Usa el componente BarLoader y personaliza sus propiedades
 };
 
 export default Loader;
--- src/components/imageUploader.jsx
+++ src/components/imageUploader.jsx
@@ -7,6 +7,7 @@ import dragAndDropIcon from './drag-and-drop.svg';
 const ImageUploader = () => {
   const [selectedImage, setSelectedImage] = useState(null);
   const [isLoading, setIsLoading] = useState(false);
+  const [isImageLoaded, setIsImageLoaded] = useState(false);
   const fileInputRef = useRef(null);
 
   const handleDrop = (event) => {
@@ -22,10 +23,10 @@ const ImageUploader = () => {
 
   const uploadImage = (image) => {
     setIsLoading(true);
-  
+
     const formData = new FormData();
     formData.append('image', image);
-  
+
     fetch('http://localhost:3001/upload', {
       method: 'POST',
       body: formData
@@ -34,65 +35,71 @@ const ImageUploader = () => {
       .then(data => {
         setSelectedImage(image);
         setIsLoading(false);
+        setIsImageLoaded(true);
       })
       .catch(error => {
         console.error('Error uploading image:', error);
         setIsLoading(false);
       });
   };
-  
+
 
   const handleCopyClick = () => {
     const imageUrl = selectedImage; // Suponiendo que `selectedImage` contiene la URL de la imagen
-  
+
     const tempInput = document.createElement('input');
     tempInput.value = imageUrl;
     document.body.appendChild(tempInput);
-  
+
     tempInput.select();
     document.execCommand('copy');
-  
+
     document.body.removeChild(tempInput);
   };
-  
+
 
   return (
     <div className="container">
-        <div className="card">
-      <h1 className='title' >Image Uploader</h1>
-      <img src={dragAndDropIcon} alt="Drag and Drop" className="drag-and-drop-icon" />
-      <div
-        className="drop-zone"
-        onDrop={handleDrop}
-        onDragOver={(event) => event.preventDefault()}
-      >
-        {isLoading ? (
-          <Loader />
-        ) : (
-          <>
-            {selectedImage ? (
-              <>
-                <ImagePreview image={selectedImage} />
-                <button className="custom-button" onClick={handleCopyClick}>Copy URL</button>
-              </>
+      <div className="card">
+        <h1 className='title'>Carga tu imagen</h1>
+        <h2>Archivo debe ser jpg, png...</h2>
+        <div
+          className="drop-zone"
+          onDrop={handleDrop}
+          onDragOver={(event) => event.preventDefault()}
+        >
+          {isLoading ? (
+            <Loader />
             ) : (
-              <>
-                <p className='text' >Selecciona una imagen de tu carpeta</p>
-                <input
-                  ref={fileInputRef}
-                  type="file"
-                  accept="image/*"
-                  onChange={handleFileInputChange}
-                  className="hidden-input"
-                />
-                <button className="custom-button" onClick={() => fileInputRef.current.click()}>
-                  Seleccionar Archivo
-                </button>
-              </>
-            )}
-          </>
-        )}
-      </div>
+                <>
+                  {isImageLoaded ? (
+                    <div className="card">
+                      <div className="image-container">
+                        <ImagePreview image={selectedImage} />
+                      </div>
+                      <p>Foto cargada correctamente</p>
+                      <button className="custom-button" onClick={handleCopyClick}>Copy URL</button>
+                    </div>
+                  ) : (
+                <>
+                  <p className='text'>Arrastra y suelta una imagen de tu carpeta</p>
+                  <img src={dragAndDropIcon} alt="Drag and Drop" className="drag-and-drop-icon" />
+                  <p className='text'>O también puedes:</p>
+                  <input
+                    ref={fileInputRef}
+                    type="file"
+                    accept="image/*"
+                    onChange={handleFileInputChange}
+                    className="hidden-input"
+                  />
+                  <button className="custom-button" onClick={() => fileInputRef.current.click()}>
+                    Seleccionar Archivo
+                  </button>
+                </>
+              )}
+            </>
+          )}
+        </div>
       </div>
     </div>
   );
